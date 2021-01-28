6d93e74f3daac08677e8bda3ec08915f8e50c2cf
George Boot <george@famboot.nl>
2021-01-28 21:11:02 +0100

    adding js tests

--- js-tests/Connector.test.ts
+++ js-tests/Connector.test.ts
@@ -1,10 +1,15 @@
 import WS from "jest-websocket-mock";
 import {Connector} from "../js-src/Connector";
+import {Channel} from "../js-src/Channel";
 
 describe('Connector', () => {
-    let server;
+    let server: WS;
 
     beforeEach(() => {
+        if (server) {
+            server.close()
+        }
+
         server = new WS("ws://localhost:1234");
     });
 
@@ -16,9 +21,37 @@ describe('Connector', () => {
         await server.connected;
 
         await expect(server).toReceiveMessage('{"event":"whoami"}');
-
         server.send('{"event":"whoami","data":{"socket_id":"test-socket-id"}}')
 
         expect(connector.socketId()).toBe('test-socket-id')
+    })
+
+    test('we can subscribe to a channel and listen to events', async () => {
+        const connector = new Connector({
+            host: "ws://localhost:1234",
+        })
+
+        await server.connected;
+
+        await expect(server).toReceiveMessage('{"event":"whoami"}');
+        server.send('{"event":"whoami","data":{"socket_id":"test-socket-id"}}')
+
+        const channel = connector.channel('my-test-channel')
+
+        await expect(server).toReceiveMessage('{"event":"subscribe","data":{"channel":"my-test-channel"}}');
+
+        server.send('{"event":"subscription_succeeded","channel":"my-test-channel"}')
+
+        expect(channel).toBeInstanceOf(Channel)
+
+        const handler1 = jest.fn();
+        const handler2 = jest.fn();
+
+        channel.on('my-test-event', handler1)
+
+        server.send('{"event":"my-test-event","channel":"my-test-channel","data":{}}')
+
+        expect(handler1).toBeCalled();
+        expect(handler2).not.toBeCalled();
     })
 });
