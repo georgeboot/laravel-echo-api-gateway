77ba6d55f3321e138b8e8b758b0273c190368f87
Michael Aguiar <mike@aliasproject.com>
2024-08-23 11:44:57 -0700

    add token back to config

--- config/laravel-echo-api-gateway.php
+++ config/laravel-echo-api-gateway.php
@@ -4,6 +4,7 @@ return [
     "connection" => [
         "key" => env("AWS_ACCESS_KEY_ID"),
         "secret" => env("AWS_SECRET_ACCESS_KEY"),
+        "token" => env("AWS_SESSION_TOKEN"),
         "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
     ],
 
