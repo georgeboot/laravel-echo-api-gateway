2b0d755e3dc021aecde37d5e0fbe87052e492b6d
Michael Aguiar <mike@aliasproject.com>
2024-08-24 08:08:15 -0700

    format fix

--- config/laravel-echo-api-gateway.php
+++ config/laravel-echo-api-gateway.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    "connection" => [
-        "key" => env("AWS_ACCESS_KEY_ID"),
-        "secret" => env("AWS_SECRET_ACCESS_KEY"),
-        "token" => env("AWS_SESSION_TOKEN"),
-        "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
+    'connection' => [
+    'key' => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    'token' => env('AWS_SESSION_TOKEN'),
+    'region' => env('AWS_DEFAULT_REGION", "us-east-1'),
     ],
 
-    "api" => [
-        "id" => env("LARAVEL_ECHO_API_GATEWAY_API_ID"),
-        "stage" => env("LARAVEL_ECHO_API_GATEWAY_API_STAGE"),
+    'api' => [
+        'id' => env('LARAVEL_ECHO_API_GATEWAY_API_ID'),
+        'stage' => env('LARAVEL_ECHO_API_GATEWAY_API_STAGE'),
     ],
 
-    "dynamodb" => [
-        "endpoint" => env("DYNAMODB_ENDPOINT"),
-        "table" => env(
-            "LARAVEL_ECHO_API_GATEWAY_DYNAMODB_TABLE",
-            "connections"
+    'dynamodb' => [
+        'endpoint' => env('DYNAMODB_ENDPOINT'),
+        'table' => env(
+        'LARAVEL_ECHO_API_GATEWAY_DYNAMODB_TABLE',
+        'connections'
         ),
     ],
 ];
