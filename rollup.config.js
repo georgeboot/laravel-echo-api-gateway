a2e4c4479f5e6b6d9b60b8fa39ec584c10cf8ad0
leob <leobdev@gmail.com>
2024-12-01 00:56:44 +0800

    attempt to get 'import axios' working, follup on PR 34

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 
+import rollupNodeResolve from 'rollup-plugin-node-resolve';
+import rollupJson from 'rollup-plugin-json';
+import rollupCommonjs from 'rollup-plugin-commonjs';
+
 export default {
     input: './js-src/Connector.ts',
     output: [
@@ -9,6 +13,15 @@ export default {
         { file: './dist/laravel-echo-api-gateway.iife.js', format: 'iife', name: 'LaravelEchoApiGateway' },
     ],
     plugins: [
+        rollupCommonjs({
+            include: 'node_modules/axios/**'
+        }),
+        rollupNodeResolve({
+          jsnext: true,
+          preferBuiltins: true,
+          browser: true
+        }),
+        rollupJson(),
         typescript(),
         babel({
             babelHelpers: 'bundled',
